32bc3a14166382ec1e59cac9299963f4b2dfb8ee
soroush <soroush.najafi89@gmail.com>
2022-12-12 12:04:07 +0330

    implement cart

--- cart/cart.py
+++ cart/cart.py
@@ -1,4 +1,6 @@
 from products.models import Product
+from django.contrib import messages
+
 
 class Cart:
     def __init__(self, request):
@@ -17,16 +19,22 @@ class Cart:
 
         self.cart = cart
 
-    def add(self, product, quantity=1):
+    def add(self, product, quantity=1, replace_current_quantity=False):
         """
         adding product to cart with specified quantity
         """
         product_id = str(product.id)
 
         if product_id not in self.cart:
-            self.cart[product_id] = {'quantity': quantity}
+            self.cart[product_id] = {'quantity': 0}
+
+        if replace_current_quantity:
+            self.cart[product_id]['quantity'] = quantity
+            messages.success(self.request, 'cart updated successfully')
+
         else:
             self.cart[product_id]['quantity'] += quantity
+            messages.success(self.request, 'Product added successfully')
 
         self.save()
 
@@ -38,6 +46,7 @@ class Cart:
 
         if product_id in self.cart:
             del self.cart[product_id]
+            messages.success(self.request, 'product removed successfully')
             self.save()
 
     def save(self):
@@ -59,23 +68,25 @@ class Cart:
             cart[str(product.id)]['product_obj'] = product
 
         for item in cart.values():
+            item['total_price'] = item['product_obj'].price * item['quantity']
             yield item
 
     def __len__(self):
         """
         this is for knowing how many products are there in the cart
         """
-        return len(self.cart.keys())
+        return sum(item['quantity'] for item in self.cart.values())
 
     def clear(self):
         """
         for clearing the cart totally
         """
         del self.session['cart']
+        messages.success(self.request, 'cart cleared successfully')
         self.save()
 
     def get_total_price(self):
         product_ids = self.cart.keys()
         products = Product.objects.filter(id__in=product_ids)
 
-        return sum(product.price for product in products)
+        return sum(item['product_obj'].price * item['quantity'] for item in self.cart.values())
