d8a752d0cbea65dc160640d8ca75fb03b386c994
soroush <soroush.najafi89@gmail.com>
2022-12-23 13:06:45 +0330

    contact model created

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Contact
+
+
+@admin.register(Contact)
+class ContactUsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'datetime_created')
--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import HomePageView
+from .views import HomePageView, ContactUsView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('contact/', ContactUsView.as_view(), name='contact_us'),
 ]
