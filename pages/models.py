887d8be87c8edea723d9a64c5a121e3f8563f190
soroush <soroush.najafi89@gmail.com>
2023-01-13 11:46:07 +0330

    contact model verbose name added

--- pages/models.py
+++ pages/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Contact(models.Model):
-    name = models.CharField(max_length=200)
-    email = models.EmailField()
+    name = models.CharField(verbose_name='Name', max_length=200)
+    email = models.EmailField(verbose_name='E-mail')
     message = models.TextField()
 
     datetime_created = models.DateTimeField(auto_now_add=True, null=True)
