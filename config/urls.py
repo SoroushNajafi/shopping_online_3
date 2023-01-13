260cd1144d7b4bd79c43d5f334c89863d6f4be57
soroush <soroush.najafi89@gmail.com>
2023-01-13 11:59:25 +0330

    select languages added

--- config/urls.py
+++ config/urls.py
@@ -21,13 +21,16 @@ from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pages.urls')),
-    path('accounts/', include('allauth.urls')),
-    path('products/', include('products.urls')),
-    path('cart/', include('cart.urls')),
-    path('order/', include('orders.urls')),
+
 
     #rosetta (i18n)
     path('rosetta/', include('rosetta.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+urlpatterns += i18n_patterns(
+    path('', include('pages.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('products/', include('products.urls')),
+    path('cart/', include('cart.urls')),
+    path('order/', include('orders.urls')),
+)
